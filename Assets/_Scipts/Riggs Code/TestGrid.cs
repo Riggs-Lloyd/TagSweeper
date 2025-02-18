8c38204494bdbeef5f473e049e6b9764a16aa796
Riggs <jriggslloyd@icloud.com>
2025-02-18 13:14:27 -0700

    messed around with block layout stuff

---
+++ Assets/_Scipts/Riggs Code/TestGrid.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGrid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        BlockLayout grid = new BlockLayout(2,2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
